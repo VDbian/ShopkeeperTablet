655d916f7b58336caa700be23bc9f1867f00fe06
AAA <1136853044@qq.com>
2018-07-02 09:50:57 +0800

    6-27

---
+++ app/src/main/java/com/administrator/shopkeepertablet/view/ui/fragment/ParishFoodFragment.java
@@ -0,0 +1,17 @@
+package com.administrator.shopkeepertablet.view.ui.fragment;
+
+import com.administrator.shopkeepertablet.di.app.AppComponent;
+import com.administrator.shopkeepertablet.view.ui.BaseFragment;
+
+/**
+ * Description:
+ * Author chencheng
+ * Time 2018/6/27
+ */
+
+public class ParishFoodFragment extends BaseFragment {
+    @Override
+    protected void setupFragmentComponent(AppComponent appComponent) {
+
+    }
+}
